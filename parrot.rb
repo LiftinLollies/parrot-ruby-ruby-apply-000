6ad587f9d5c2b0bb48ba35951bea3f819357fd88
Liftin Lollies <lordlayer000@gmail.com>
2020-03-13 15:28:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase ="Squawk!")
+def parrot(phrase ="Polly wants a cracker")
   puts phrase
   phrase
 end
